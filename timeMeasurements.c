1f287c19aac2d579f719ba57a9306d952c5f1dbc
WJKPK <krupskiwojciech@gmail.com>
2019-01-02 22:46:04 +0100

    Update and add prints in timeMeasurements.c

--- timeMeasurements.c
+++ timeMeasurements.c
@@ -1,7 +1,7 @@
 #include"binary_s.h"
 #include <time.h>
 #define SIZE 10
-#define MAXVAL 100000000
+#define MAXVAL 3000000
 #define NUM_ELEM_UNSORT_ARR 10000
 #define NUM_OF_REP 10000
 
@@ -10,7 +10,7 @@ POINTER = NULL;} while(0)
 
 #define TIME_MEASUREMENT_SEARCH(FUNCTION, INPUT_ARRAY, NUM_ARR_ELEM) do{start = clock();\
 	for(i = 0; i < NUM_ARR_ELEM; i += NUM_ARR_ELEM/NUM_OF_REP)\
-		FUNCTION(i, INPUT_ARRAY, NUM_ARR_ELEM);\
+	FUNCTION(i, INPUT_ARRAY, NUM_ARR_ELEM);\
 	stop = clock();\
 	elapsed = (double)(stop - start) * 1000.0 / CLOCKS_PER_SEC;} while(0)
 
@@ -31,11 +31,15 @@ static void fillRandom(int* tab, int numberOfElem)
 
 int main()
 {
-	int arrayOfNumbers[SIZE] = {3, 3, 3, 7, 7, 13, 18, 21, 25, 27};
+	int arrayOfNumbers[SIZE] = {2, 3, 3, 7, 7, 13, 18, 21, 25, 27};
 	printf("\nGiven is an array: 3, 3, 3, 7, 7, 13, 18, 21, 25, 27\n");
-	printf("Search 3 using a lowerbound binary search: %d\n", binarySearchLowerBound(3, arrayOfNumbers, SIZE));
-	printf("Search 3 using a upperbound binary search: %d\n", binarySearchUpperBound(3, arrayOfNumbers, SIZE));
-	printf("\nMeasurment for %d elements in unsorted array.\n", NUM_ELEM_UNSORT_ARR);
+	printf("Search element with value 4 using a lowerbound binary search: %d\n", binarySearchLowerBound(4, arrayOfNumbers, SIZE));
+	printf("Search element with value 4 using a upperbound binary search: %d\n", binarySearchUpperBound(4, arrayOfNumbers, SIZE));
+	printf("Search element with value 4 using a binary search: %d\n", binarySearch(4, arrayOfNumbers, SIZE));
+	printf("Search element with value 2 using a binary search: %d\n", binarySearch(2, arrayOfNumbers, SIZE));
+	printf("Search element with value 3 using a binary search: %d\n", binarySearch(3, arrayOfNumbers, SIZE));
+	printf("Search element with value 13 using a binary search: %d\n", binarySearch(13, arrayOfNumbers, SIZE));
+	printf("\nMeasurement for %d elements in unsorted array.\n", NUM_ELEM_UNSORT_ARR);
 	srand(time(0));
 	clock_t start, stop;
 	double elapsed;
@@ -49,7 +53,7 @@ int main()
 	stop = clock();
 	elapsed = (double)(stop - start) * 1000.0 / CLOCKS_PER_SEC;
 	FREE(histogram);
-	printf("Time to count sort a %d element array [ms]: %f\n", NUM_ELEM_UNSORT_ARR, elapsed);
+	printf("Time to count sort an array containing %d elements [ms]: %f\n", NUM_ELEM_UNSORT_ARR, elapsed);
 
 	fillRandom(arrayWithRandNum, NUM_ELEM_UNSORT_ARR);
 	start = clock();
@@ -57,23 +61,23 @@ int main()
 	stop = clock();
 	elapsed = (double)(stop - start) * 1000.0 / CLOCKS_PER_SEC;
 	FREE(histogram);
-	printf("Time to qsort a %d element array [ms]: %f\n", NUM_ELEM_UNSORT_ARR, elapsed);
+	printf("Time to qsort an array containing %d elements  [ms]: %f\n", NUM_ELEM_UNSORT_ARR, elapsed);
 
 
 	start = clock();
 	bubbleSort(arrayWithRandNum, NUM_ELEM_UNSORT_ARR);
 	stop = clock();
 	elapsed = (double)(stop - start) * 1000.0 / CLOCKS_PER_SEC;
-	printf("Time to bubbleSort a %d element array [ms]: %f\n", NUM_ELEM_UNSORT_ARR, elapsed);
+	printf("Time to bubble sort an array containing %d elements [ms]: %f\n", NUM_ELEM_UNSORT_ARR, elapsed);
 
 	fillRandom(arrayWithRandNum, NUM_ELEM_UNSORT_ARR);
 	TIME_MEASUREMENT_SEARCH(linearSearch, arrayWithRandNum, NUM_ELEM_UNSORT_ARR);
-	printf("Time to %d linear search in a %d element array [ms]: %f\n", NUM_ELEM_UNSORT_ARR, NUM_ELEM_UNSORT_ARR, elapsed);
+	printf("\nTime to %d linear search in a %d element array [ms]: %f\n", NUM_ELEM_UNSORT_ARR, NUM_ELEM_UNSORT_ARR, elapsed);
 
 	fillRandom(arrayWithRandNum, NUM_ELEM_UNSORT_ARR);
 	histogram = countSort(arrayWithRandNum, NUM_ELEM_UNSORT_ARR, MAXVAL);
 	TIME_MEASUREMENT_SEARCH(binarySearch, arrayWithRandNum, NUM_ELEM_UNSORT_ARR);
-	printf("Time to %d binary search (+ count sort) a %d element array [ms]: %f\n", NUM_ELEM_UNSORT_ARR, NUM_ELEM_UNSORT_ARR, elapsed);
+	printf("Time to %d binary search a %d element array [ms]: %f\n", NUM_ELEM_UNSORT_ARR, NUM_ELEM_UNSORT_ARR, elapsed);
 
 	FREE(arrayWithRandNum);
 	FREE(histogram);
